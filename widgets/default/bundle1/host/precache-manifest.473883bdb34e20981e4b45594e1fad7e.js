1ce2b9f6096779543d60c6078ae91b04c9188b2e
Alexander <example@mail.com>
2024-11-24 09:04:27 +0300

    First working widgets bundle

---
+++ widgets/default/bundle1/host/precache-manifest.473883bdb34e20981e4b45594e1fad7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "75c84d1aa72fff84f7ac",
+    "url": "./static/css/main.70a469cf.chunk.css"
+  },
+  {
+    "revision": "75c84d1aa72fff84f7ac",
+    "url": "./static/js/main.75c84d1a.chunk.js"
+  },
+  {
+    "revision": "44c0225b478fe0053167",
+    "url": "./static/js/1.44c0225b.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "924b6b21a34b19a4c7a12aa8eced6522",
+    "url": "./index.html"
+  }
+];
